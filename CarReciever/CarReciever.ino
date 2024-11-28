c8de83675421919068df5361b9d2d6e362c476f7
ZeK1ng <dgogi17@freeuni.edu.ge>
2024-11-28 20:30:11 +0400

    added transmitter and reciever. works fine. sending cords

---
+++ CarReciever/CarReciever.ino
@@ -0,0 +1,40 @@
+#include "nRF24L01.h"
+#include "RF24.h"
+#include <SPI.h>
+
+#define CE_PIN 7 
+#define CSN_PIN 8
+#define CHANNEL_OFFSET 56
+
+const byte address[6] = "00001";
+RF24 radio(CE_PIN, CSN_PIN);
+char data[10];
+struct Cords{
+  int x;
+  int y;
+};
+Cords cords;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  radio.begin();
+  radio.setAutoAck(false);  
+  radio.setChannel(CHANNEL_OFFSET);
+  radio.setPALevel (RF24_PA_MIN); //transmitter power level. To choose RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+	radio.setDataRate(RF24_250KBPS); //(RF24_250KBPS|RF24_1MBPS|RF24_2MBPS) 
+  radio.openReadingPipe(1,address);
+  radio.startListening();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(radio.available()){
+    while(radio.available()){
+        radio.read(&cords, sizeof(cords));
+    }
+    Serial.print("X = ");
+    Serial.print(cords.x);
+    Serial.print(" Y = ");  
+    Serial.println(cords.y);
+  }
+}
---
+++ Joystick/Joystick.ino
@@ -0,0 +1,81 @@
+#include "nRF24L01.h"
+#include "RF24.h"
+#include <SPI.h>
+
+#define CE_PIN  9
+#define CSN_PIN 10
+#define INTERVAL_MS_TRANSMISSION 250 
+#define CHANNEL_OFFSET 56
+const byte address[6] = "00001"; 
+char data[100] = "hi";
+struct JoystickPinConfig {
+  int x_axis = A0;
+  int y_axis = A1;
+  int up = 2;
+  int down =4;
+  int left = 5;
+  int right = 3;
+};
+JoystickPinConfig joystickPinConfig;
+
+struct Joystick{
+  int x_axis;
+  int y_axis;
+  int up;
+  int down;
+  int left;
+  int right;
+};
+Joystick joystick;
+
+struct Cords{
+  int x;
+  int y;
+};
+Cords cords;
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+void setupRadio(){
+  radio.begin(); 
+  radio.setAutoAck(false);  
+  radio.setChannel(CHANNEL_OFFSET);
+  radio.openWritingPipe(address); 
+  radio.setPALevel (RF24_PA_MIN); //transmitter power level. To choose RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+	radio.setDataRate(RF24_250KBPS); //(RF24_250KBPS|RF24_1MBPS|RF24_2MBPS) 
+  radio.stopListening(); 
+}
+
+void setupJoystick(){
+  pinMode(joystickPinConfig.up, INPUT_PULLUP);
+  pinMode(joystickPinConfig.down, INPUT_PULLUP);
+  pinMode(joystickPinConfig.left, INPUT_PULLUP);
+  pinMode(joystickPinConfig.right, INPUT_PULLUP);
+  
+  digitalWrite(joystickPinConfig.up, LOW);
+  digitalWrite(joystickPinConfig.down, LOW);
+  digitalWrite(joystickPinConfig.left, LOW);
+  digitalWrite(joystickPinConfig.right, LOW);
+}
+void setup() {
+  Serial.begin(9600); 
+  setupRadio();
+	setupJoystick();
+}
+void printInputs(){
+  cords.x = analogRead(joystickPinConfig.x_axis);
+  cords.y = analogRead(joystickPinConfig.y_axis);
+  Serial.print("X = ");
+  Serial.print(cords.x);
+  Serial.print(" Y = ");  
+  Serial.println(cords.y);
+}
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    printInputs();
+    radio.write(&cords, sizeof(cords));
+    delay(200);
+}
