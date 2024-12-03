ffb0bee8334d1ed9fa654ad87f434ba6d95c4676
ZeK1ng <dgogi17@freeuni.edu.ge>
2024-12-03 21:50:39 +0400

    added logic for handling movement. Did circuiting with just the wheels and connections, no solid surface yet. Have to run full test with transmitter

--- source/CarReciever/CarReciever.ino
+++ source/CarReciever/CarReciever.ino
@@ -6,14 +6,40 @@
 #define CE_PIN 7 
 #define CSN_PIN 8
 const byte  address[6];
+const int DELAY = 1000;
+const STICK_X_CENTER = 330;
+const STICK_Y_CENTER = 330;
+const STICK_MARGIN =  10;
+
 
 RF24 radio(CE_PIN, CSN_PIN);
 
+struct MotorBridge{
+  int motorRightPin1 = 2;
+  int motorRightPin2 = 3;
+  int motorLeftPin1 = 4;
+  int motorLeftPin2 = 5;
+};
+
+MotorBridge motorBridge;
 Cords cords;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  setupMotor();
+  // setupRadio();
+}
+void setupMotor(){
+  pinMode(motorBridge.motorRightPin1,OUTPUT);
+  pinMode(motorBridge.motorRightPin2,OUTPUT);
+  pinMode(motorBridge.motorLeftPin1,OUTPUT);
+  pinMode(motorBridge.motorLeftPin2,OUTPUT);
+  leftStop();
+  rightStop();
+}
+
+void setupRadio(){
   radio.begin();
   radio.setAutoAck(false);  
   radio.setChannel(CHANNEL_OFFSET);
@@ -25,13 +51,92 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  listenToRadio();
+  handleInput();
+  delay(DELAY);
+}
+
+void printCords(){
+  Serial.print("X = ");
+  Serial.print(cords.x);
+  Serial.print(" Y = ");  
+  Serial.println(cords.y);
+}
+
+void handleInput(){
+  if(cords.x > STICK_X_CENTER - STICK_MARGIN){
+    printCords();
+    moveForward();
+  }else
+  if(cords.x < STICK_X_CENTER - STICK_MARGIN){
+    printCords();
+    moveBackward();
+  }else
+  if(cords.y > STICK_Y_CENTER + STICK_MARGIN){
+    printCords();
+    moveRight();
+  }else
+  if(cords.y < STICK_Y_CENTER - STICK_MARGIN){
+    printCords();
+    moveLeft();
+  }
+}
+
+void leftForward(){
+  digitalWrite(motorBridge.motorLeftPin1, LOW);
+  digitalWrite(motorBridge.motorLeftPin2, HIGH);
+}
+
+void rightForward(){
+  digitalWrite(motorBridge.motorRightPin1, LOW);
+  digitalWrite(motorBridge.motorRightPin2, HIGH);
+
+}
+void leftBackward(){
+  digitalWrite(motorBridge.motorLeftPin1, HIGH);
+  digitalWrite(motorBridge.motorLeftPin2, LOW);
+}
+
+void rightBackward(){
+  digitalWrite(motorBridge.motorRightPin1, HIGH);
+  digitalWrite(motorBridge.motorRightPin2, LOW);
+}
+
+void leftStop(){
+  digitalWrite(motorBridge.motorLeftPin1, LOW);
+  digitalWrite(motorBridge.motorLeftPin2, LOW);
+}
+
+void rightStop(){
+  digitalWrite(motorBridge.motorRightPin1, LOW);
+  digitalWrite(motorBridge.motorRightPin2, LOW);
+}
+
+void moveForward(){
+  rightForward();
+  leftForward();
+}
+
+void moveBackward(){
+  rightBackward();
+  leftBackward();
+}
+
+void moveLeft(){
+  leftStop();
+  rightForward();
+}
+
+void moveRight(){
+  rightStop();
+  leftForward();
+}
+
+void listenToRadio(){
   if(radio.available()){
     while(radio.available()){
         radio.read(&cords, sizeof(cords));
     }
-    Serial.print("X = ");
-    Serial.print(cords.x);
-    Serial.print(" Y = ");  
-    Serial.println(cords.y);
+    printCords();
   }
 }
